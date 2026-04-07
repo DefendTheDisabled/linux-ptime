78e9f4df080c75da345978a0c4b434a79b009cda
Sean Smith <DefendTheDisabled@gmail.com>
2026-04-07 16:45:46 -0500

    v2.0: Update xfstests ptime_set.c and helpers for file_setattr API

--- tests/xfstests/src/ptime_set.c
+++ tests/xfstests/src/ptime_set.c
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * ptime_set - Set provenance time (ptime) on a file via utimensat
+ * ptime_set - Set provenance time (ptime) on a file via file_setattr
  *
  * Usage: ptime_set <file> <epoch_sec> [nsec]
  *   Sets ptime to the given epoch seconds (and optional nanoseconds).
- *   atime and mtime are left unchanged (UTIME_OMIT).
+ *   Only ptime is modified; atime/mtime/xflags are not touched.
  *
- * Usage: ptime_set <file>
- *   Clears ptime by setting it to 0.0 (if supported).
+ * Uses file_setattr syscall (469) with struct file_attr VER1 (40 bytes).
+ * ptime_sec at offset 24, ptime_nsec at offset 32, ptime_pad at offset 36.
  */
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -17,21 +17,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/syscall.h>
+#include <stdint.h>
 
-#ifndef AT_UTIME_PTIME
-#define AT_UTIME_PTIME 0x20000
-#endif
-
-#ifndef UTIME_OMIT
-#define UTIME_OMIT ((1L << 30) - 2L)
+#ifndef SYS_file_setattr
+#define SYS_file_setattr 469
 #endif
 
 int main(int argc, char *argv[])
 {
-	struct timespec times[3];
 	const char *path;
-	long sec = 0;
-	long nsec = 0;
+	int64_t sec = 0;
+	uint32_t nsec = 0;
+	char fa[40];
 	int ret;
 
 	if (argc < 3) {
@@ -42,21 +39,17 @@ int main(int argc, char *argv[])
 	path = argv[1];
 	sec = atol(argv[2]);
 	if (argc >= 4)
-		nsec = atol(argv[3]);
+		nsec = (uint32_t)atol(argv[3]);
 
-	/* UTIME_OMIT for atime and mtime */
-	times[0].tv_sec = 0;
-	times[0].tv_nsec = UTIME_OMIT;
-	times[1].tv_sec = 0;
-	times[1].tv_nsec = UTIME_OMIT;
-	/* ptime */
-	times[2].tv_sec = sec;
-	times[2].tv_nsec = nsec;
+	/* Build file_attr VER1 struct: zero VER0 fields, set ptime */
+	memset(fa, 0, 40);
+	memcpy(fa + 24, &sec, 8);   /* fa_ptime_sec */
+	memcpy(fa + 32, &nsec, 4);  /* fa_ptime_nsec */
 
-	ret = syscall(SYS_utimensat, AT_FDCWD, path, times, AT_UTIME_PTIME);
+	ret = syscall(SYS_file_setattr, AT_FDCWD, path, fa, (size_t)40, 0);
 	if (ret != 0) {
-		fprintf(stderr, "utimensat(%s, ptime=%ld.%09ld): %s\n",
-			path, sec, nsec, strerror(errno));
+		fprintf(stderr, "file_setattr(%s, ptime=%ld.%09u): %s\n",
+			path, (long)sec, nsec, strerror(errno));
 		return 1;
 	}
 	return 0;
